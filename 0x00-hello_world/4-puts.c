02be26f618bc6570e6cac1d692fe6265a6fd7739
TheSaviourEking <106761645+TheSaviourEking@users.noreply.github.com>
2022-07-01 03:31:49 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Makes the sum of two numbers
+ * main - prints out the paranthesized text
  * Description: This is a longer description.
  * Don't forget that a line should not exceed 80 characters.
  * But you're totally free to use several lines to properly
  * describe your function
- * Return: The sum of the two parameters
+ * Return: The paranthesized characters
  */
 int main(void)
 {
