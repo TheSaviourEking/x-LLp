d162da9859017f480a147a85a78e3ea33f448f8f
TheSaviourEking <106761645+TheSaviourEking@users.noreply.github.com>
2022-07-01 15:47:58 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,12 +6,9 @@
 *executed and prints based n conditions
 *Return : always 0
 */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
