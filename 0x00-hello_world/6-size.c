301eb4227d8bcea64a46454c39dc922fb36df713
TheSaviourEking <106761645+TheSaviourEking@users.noreply.github.com>
2022-07-01 03:44:53 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
-*main: prints the size of various files
-*Return 0 is exited properly;otherwise non zero 
-*/
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
