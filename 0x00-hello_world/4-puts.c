4645c9068f53a3e4708802dcbb45964016948915
TheSaviourEking <106761645+TheSaviourEking@users.noreply.github.com>
2022-07-01 03:24:18 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - Makes the sum of two numbers
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
