e522ad1d91aa87fb997a7bfa60aa6fad012ca123
Saviour Eking <saviour.eking@outlook.com>
2022-07-05 19:17:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_putchar -> print alphabet in lower case
+* Return: 0;
+*/
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a', i <= 'z', i++)
+	{
+	       	_putchar(i);
+	}
+	_putchar('\n')
+}
