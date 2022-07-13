2bf5044a59b9a7beda8bb250ccacf2b1a6923e48
Saviour Eking <saviour.eking@outlook.com>
2022-07-13 16:44:35 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "alx.h"
+
+/**
+ * swap_int -> a function that swaps the values of two integers.
+ * @a: an inout integer pointer
+ * @b: an input integer pointer
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int new;
+	new = *a;
+	*a = *b;
+	*b = new;
+
+}
+
