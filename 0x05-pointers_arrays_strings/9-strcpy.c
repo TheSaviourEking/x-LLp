183ce3bb6aa53a801331ef211bdab72f920a8748
Saviour Eking <saviour.eking@outlook.com>
2022-07-13 19:24:24 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *new = dest;
+	int i;
 
-	while (*src)
-		*dest++ = *src++;
-	return (new);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
 }
