d9956f181eed5ee5bce9e19b1be1d05197ba2107
Saviour Eking <saviour.eking@outlook.com>
2022-07-14 17:41:00 -0700

    0-strcat-concatenate 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "alx.h"
+
+/**
+ * _strcat -> a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	chat *temp = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# 0x06.C-More pointers,arrays and strings
+---
+Continued exercises on Alx low level Programming projects 
+------
+------
+*** Alx Rocks ***
