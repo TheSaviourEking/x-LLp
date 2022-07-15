07c3d0f332ce647ee2c7ba5a99df865094d7d0ee
Saviour Eking <saviour.eking@outlook.com>
2022-07-14 18:13:51 -0700

    2-strncpy.c - copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "alx.h"
+
+/**
+ * _strncpy -> a function that copy's the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srclen = 0;
+	int i = 0;
+	char *temp = dest;
+	char *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	srclen++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
+}
