a9046c4416be4f3035098839964c9eba5adda241
Saviour Eking <saviour.eking@outlook.com>
2022-07-15 18:20:00 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* print_alphabet -> prints the lowercase alphabets
+* print_alphabet -> prints the lowercase alphabets followed by new lines
+* Return: Nothing
 */
 
 void print_alphabet(void)
 {
-	int j;
+	char j;
 
 	for (j = 'a'; j <= 'z'; j++)
 	{
